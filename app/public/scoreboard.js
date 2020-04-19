d2e251a9e524ccd2c94c41f4e293f7c00359072f
Mitchell-Johnson <mitchj9@uw.edu>
2020-04-19 14:00:02 -0700

    finished

--- app/public/scoreboard.js
+++ app/public/scoreboard.js
@@ -4,20 +4,20 @@ const scoreboard = document.getElementById("scoreboardDiv");
 function getScores() {
     fetch("/api/scoreboard")
     .then((data) => {
-        return data;
+        return data.json();
     }).then((response) => {
         appendScores(response);
+        console.log(response);
     });
 }
 
 // appends the retrieved scoreboard data to the scoreboard div
 function appendScores(data) {
-    const scores = data.scores;
-    scores.forEach(function(entry) {
+    for(var i = 0; i < data.length; i++) {
         const list = document.createElement("li");
-        list.textContent = `${entry.name} - Score: ${entry.score}`;
+        list.textContent = `${data[i].name} - Score: ${data[i].score}`;
         scoreboard.appendChild(list);
-    });
+    };
 }
 
 getScores();
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -24,6 +24,7 @@ module.exports = function(app) {
     connection.query(dbQuery, function(err, result) {
       if (err) throw err;
       res.json(result);
+      console.log(result);
     });
   });
 };
